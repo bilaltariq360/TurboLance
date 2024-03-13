1a9677148cda00cfb0b05c5cee9c542a186009e8
bilaltariq360 <hafizbilaltariq360@gmail.com>
2024-03-13 23:56:06 +0500

    login page validation

--- turbolance/src/component/Signin.js
+++ turbolance/src/component/Signin.js
@@ -3,11 +3,42 @@ import React from "react";
 import logoim from "../img/Logo3.png";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Signin() {
+  const notify = (e) => {
+    let email = document.getElementById("email").value;
+    let validEmail = String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+    let password = document.getElementById("password").value;
+    let errMsg = "";
+
+    if (!validEmail) errMsg = "Please enter valid email";
+    else if (!password) errMsg = "Please enter password";
+    else if (password.length < 6) errMsg = "Please enter correct password";
+
+    if (errMsg) {
+      toast.error(errMsg, {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      e.preventDefault();
+    }
+  };
   return (
     <>
       <Navbar />
+      <ToastContainer />
       <div className="flex justify-center items-center min-h-screen">
         <div className="max-w-md w-full px-6 py-8 bg-white shadow-md rounded-lg">
           <img src={logoim} alt="logo" className="px-14" />
@@ -17,9 +48,10 @@ function Signin() {
           <p className="text-center text-gray-600 mt-2">
             Use your TurboLancer Account
           </p>
-          <form className="mt-8 space-y-4">
+          <form className="mt-8 space-y-4" onSubmit={notify}>
             <div>
               <input
+                id="email"
                 type="text"
                 className="border-2 w-full px-4 py-2 rounded-md focus:outline-none focus:border-blue-400"
                 placeholder="Email"
@@ -27,6 +59,7 @@ function Signin() {
             </div>
             <div>
               <input
+                id="password"
                 type="password"
                 className="border-2 w-full px-4 py-2 rounded-md focus:outline-none focus:border-blue-400"
                 placeholder="Password"
