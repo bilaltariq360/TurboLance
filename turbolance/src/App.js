cff5fa7c23093c78950bd98948c8301899dbf050
bilaltariq360 <hafizbilaltariq360@gmail.com>
2024-03-13 19:57:27 +0500

    home page added

---
+++ turbolance/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+
+import Home from "./component/Home";
+import Signup from "./component/Signup";
+import Signin from "./component/Signin";
+import Error from "./component/404";
+import Gigs from "./component/Gigs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
+import Dashboard from "./component/Dashboard";
+import Documentation from "./component/Documentation";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Gigs" element={<Gigs />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="Footer" element={<Footer />} />
+        <Route path="/Documentation" element={<Documentation />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
